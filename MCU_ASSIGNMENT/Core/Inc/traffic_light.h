4d3bdc7ffa17f655a477c17700bf51eb5db27956
locle22 <loc.le221025@hcmut.edu.vn>
2025-12-06 14:26:30 +0700

    adding traffic_light.h to DISPLAY_UTILITY

--- MCU_ASSIGNMENT/Core/Inc/traffic_light.h
+++ MCU_ASSIGNMENT/Core/Inc/traffic_light.h
@@ -7,7 +7,23 @@
 
 #ifndef INC_TRAFFIC_LIGHT_H_
 #define INC_TRAFFIC_LIGHT_H_
+#include "main.h"
+#include "software_timer.h"
 
 
+#define RED 0
+#define YELLOW 1
+#define GREEN 2
+#define OFF_STATE 3
+#define BLINK_TIME 250
+
+
+void TrafficLight1Control(int state);
+void TrafficLight2Control(int state);
+
+
+void TwoWayControl(int state_1, int state_2);
+
+void TrafficBlink(int state);
 
 #endif /* INC_TRAFFIC_LIGHT_H_ */
