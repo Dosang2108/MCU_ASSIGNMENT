970fca335e580c50a36ff065164499e6b0a44d5c
locle22 <loc.le221025@hcmut.edu.vn>
2025-12-06 14:52:53 +0700

    new config

--- MCU_ASSIGNMENT/Core/Src/main.c
+++ MCU_ASSIGNMENT/Core/Src/main.c
@@ -190,8 +190,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LD2_Pin|TRAFFIC_A0_Pin|TRAFFIC_A1_Pin|TRAFFIC_B0_Pin
-                          |TRAFFIC_B1_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, TRAFFIC_A0_Pin|TRAFFIC_A1_Pin|TRAFFIC_B0_Pin|LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(TRAFFIC_B1_GPIO_Port, TRAFFIC_B1_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -205,20 +207,25 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
+  /*Configure GPIO pins : TRAFFIC_A0_Pin TRAFFIC_A1_Pin TRAFFIC_B0_Pin LD2_Pin */
+  GPIO_InitStruct.Pin = TRAFFIC_A0_Pin|TRAFFIC_A1_Pin|TRAFFIC_B0_Pin|LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
   /*Configure GPIO pins : USART_TX_Pin USART_RX_Pin */
   GPIO_InitStruct.Pin = USART_TX_Pin|USART_RX_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : LD2_Pin TRAFFIC_A0_Pin TRAFFIC_A1_Pin TRAFFIC_B0_Pin
-                           TRAFFIC_B1_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin|TRAFFIC_A0_Pin|TRAFFIC_A1_Pin|TRAFFIC_B0_Pin
-                          |TRAFFIC_B1_Pin;
+  /*Configure GPIO pin : TRAFFIC_B1_Pin */
+  GPIO_InitStruct.Pin = TRAFFIC_B1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  HAL_GPIO_Init(TRAFFIC_B1_GPIO_Port, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
   HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
