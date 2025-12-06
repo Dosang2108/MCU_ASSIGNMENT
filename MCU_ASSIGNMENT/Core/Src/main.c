105cb4cf8e343f5b136dad41550164ebf111d055
Do Minh Sang <minhsang210805@gmail.com>
2025-12-06 11:09:50 +0700

    UPLOAD RENAME GPIO

--- MCU_ASSIGNMENT/Core/Src/main.c
+++ MCU_ASSIGNMENT/Core/Src/main.c
@@ -201,7 +201,7 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 0;
+  htim2.Init.Prescaler = 9;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim2.Init.Period = 7999;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -276,8 +276,8 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LD2_Pin|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8
-                          |GPIO_PIN_9, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LD2_Pin|TRAFFIC_A0_Pin|TRAFFIC_A1_Pin|TRAFFIC_B0_Pin
+                          |TRAFFIC_B1_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -291,10 +291,10 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : LD2_Pin PA6 PA7 PA8
-                           PA9 */
-  GPIO_InitStruct.Pin = LD2_Pin|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8
-                          |GPIO_PIN_9;
+  /*Configure GPIO pins : LD2_Pin TRAFFIC_A0_Pin TRAFFIC_A1_Pin TRAFFIC_B0_Pin
+                           TRAFFIC_B1_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin|TRAFFIC_A0_Pin|TRAFFIC_A1_Pin|TRAFFIC_B0_Pin
+                          |TRAFFIC_B1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
