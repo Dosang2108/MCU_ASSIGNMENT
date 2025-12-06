f760c49b20b03b5de3d48cb3c22506df4a2aeb40
DESKTOP-MFQSNBS\Tran Doan Hoang Lam <lam.tran2311828@hcmut.edu.vn>
2025-12-06 12:50:28 +0700

    Update button for IOC

--- MCU_ASSIGNMENT/Core/Src/main.c
+++ MCU_ASSIGNMENT/Core/Src/main.c
@@ -285,8 +285,8 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PC0 PC1 PC2 PC3 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3;
+  /*Configure GPIO pins : BUTTON1_Pin BUTTON2_Pin BUTTON3_Pin BUTTON4_Pin */
+  GPIO_InitStruct.Pin = BUTTON1_Pin|BUTTON2_Pin|BUTTON3_Pin|BUTTON4_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
