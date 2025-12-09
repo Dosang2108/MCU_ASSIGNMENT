84d51739d5c6f32332762354ae5af27a40c4829f
Do Minh Sang <minhsang210805@gmail.com>
2025-12-09 11:32:13 +0700

    update IOC

--- MCU_ASSIGNMENT/Core/Src/main.c
+++ MCU_ASSIGNMENT/Core/Src/main.c
@@ -276,10 +276,13 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, TRAFFIC_A0_Pin|TRAFFIC_A1_Pin|TRAFFIC_B0_Pin|LD2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(TRAFFIC_B1_GPIO_Port, TRAFFIC_B1_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(TRAFFIC_A0_GPIO_Port, TRAFFIC_A0_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, TRAFFIC_B0_Pin|TRAFFIC_B1_Pin|TRAFFIC_A1_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -287,25 +290,38 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : BUTTON1_Pin BUTTON2_Pin BUTTON3_Pin BUTTON4_Pin */
-  GPIO_InitStruct.Pin = BUTTON1_Pin|BUTTON2_Pin|BUTTON3_Pin|BUTTON4_Pin;
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : BUTTON2_Pin BUTTON1_Pin */
+  GPIO_InitStruct.Pin = BUTTON2_Pin|BUTTON1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : TRAFFIC_A0_Pin TRAFFIC_A1_Pin TRAFFIC_B0_Pin LD2_Pin */
-  GPIO_InitStruct.Pin = TRAFFIC_A0_Pin|TRAFFIC_A1_Pin|TRAFFIC_B0_Pin|LD2_Pin;
+  /*Configure GPIO pin : TRAFFIC_A0_Pin */
+  GPIO_InitStruct.Pin = TRAFFIC_A0_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(TRAFFIC_A0_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : BUTTON3_Pin BUTTON4_Pin */
+  GPIO_InitStruct.Pin = BUTTON3_Pin|BUTTON4_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : TRAFFIC_B1_Pin */
-  GPIO_InitStruct.Pin = TRAFFIC_B1_Pin;
+  /*Configure GPIO pins : TRAFFIC_B0_Pin TRAFFIC_B1_Pin TRAFFIC_A1_Pin */
+  GPIO_InitStruct.Pin = TRAFFIC_B0_Pin|TRAFFIC_B1_Pin|TRAFFIC_A1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(TRAFFIC_B1_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
   HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
